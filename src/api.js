cf529c14445dd248bc00b951d3c0a40f90257e5b
raisavar <raisavar@users.noreply.github.com>
2025-10-07 03:13:31 +0300

    remove(auth): fix data validation

--- src/api.js
+++ src/api.js
@@ -212,3 +212,30 @@ class API {
 
 // Update 63
 module.exports = API;
+
+
+// API module for EurekaFramework
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 65
+module.exports = API;
